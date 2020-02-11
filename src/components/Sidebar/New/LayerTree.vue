db22ad04e183450412b60f48e7ac951b6a934269
khayyam.islamzadeh <khayyam.islamzadeh@KHAYYAMI-COMP>
2020-02-11 12:16:28 +0400

    fixed

--- src/components/Sidebar/New/LayerTree.vue
+++ src/components/Sidebar/New/LayerTree.vue
@@ -1,151 +1,164 @@
 <template>
-    <li class="list__item">
-        <span class="item__header" :style="`padding-left: ${paddingLeft}`">
-            <span class="title">
-                <ToggleSwitch
-                    v-if="!data.hasOwnProperty('children')"
-                    class="pre"
-                    v-model="switchModel"
-                />
-                <i class="far fa-folder pre" v-else></i>
-                {{ data.name }}
-            </span>
+  <li class="list__item">
+    <span class="item__header" :style="`padding-left: ${paddingLeft}`">
+      <span class="title">
+        <ToggleSwitch v-if="!isCategory" class="pre" v-model="switchModel" />
+        <i class="far fa-folder pre" v-else></i>
+        {{ data.name }}
+      </span>
 
-            <!-- Caret icons -->
-            <span v-if="showCaretIcons">
-                <i
-                    class="fas fa-caret-down"
-                    v-show="subListVisibility"
-                    @click="toggleSubList"
-                />
-                <i
-                    class="fas fa-caret-up"
-                    v-show="!subListVisibility"
-                    @click="toggleSubList"
-                />
-            </span>
+      <!-- Caret icons -->
+      <span v-if="showCaretIcons">
+        <i
+          class="fas fa-caret-down"
+          v-show="subListVisibility"
+          @click="toggleSubList"
+        />
+        <i
+          class="fas fa-caret-up"
+          v-show="!subListVisibility"
+          @click="toggleSubList"
+        />
+      </span>
 
-            <!-- Color and Table icons -->
-            <span v-if="showOperationIcons" class="icons">
-                <!-- <img src="@/assets/images/icons/color_fill.svg" alt="" /> -->
-                <img
-                    src="@/assets/images/icons/list.svg"
-                    alt=""
-                    @click="
-                        $emit('getTableData', parent, data.id, data.name, {
-                            where: '1=1',
-                        })
-                    "
-                />
-            </span>
-        </span>
-        <ul v-if="data.layers && subListVisibility" class="list__content">
-            <LayerTree
-                v-for="(layer, index) in data.layers"
-                :key="index"
-                :data="layer"
-                :parent="data"
-                :loop="loop + 1"
-                @selectLayer="selectLayer"
-                @selectSubLayer="selectSubLayer"
-                @getTableData="getTableData"
-            />
-        </ul>
-    </li>
+      <!-- Color and Table icons -->
+      <span v-if="showOperationIcons" class="icons">
+        <!-- <img src="@/assets/images/icons/color_fill.svg" alt="" /> -->
+        <img
+          src="@/assets/images/icons/list.svg"
+          alt=""
+          @click="
+            $emit('getTableData', parent, data.id, data.name, {
+              where: '1=1'
+            })
+          "
+        />
+      </span>
+    </span>
+    <ul v-if="data.layers && subListVisibility" class="list__content">
+      <LayerTree
+        v-for="(children, index) in data.children"
+        :key="children.name + index"
+        :data="children"
+        :parent="data"
+        :loop="loop + 1"
+        @selectSubLayer="selectSubLayer"
+        @selectLayer="selectLayer"
+        @getTableData="getTableData"
+      />
+
+      <LayerTree
+        v-for="(layer, index) in data.layers"
+        :key="layer.name + index"
+        :data="layer"
+        :parent="data"
+        :loop="loop + 1"
+        @selectSubLayer="selectSubLayer"
+        @selectLayer="selectLayer"
+        @getTableData="getTableData"
+      />
+    </ul>
+  </li>
 </template>
 
 <script>
 import ToggleSwitch from "../../common/ToggleSwitch";
 
 export default {
-    name: "LayerTree",
-    components: {
-        ToggleSwitch,
+  name: "LayerTree",
+  components: {
+    ToggleSwitch
+  },
+  props: {
+    data: {
+      type: Object
     },
-    props: {
-        data: {
-            type: Object,
-        },
-        parent: {
-            type: Object,
-        },
-        loop: {
-            type: Number,
-            default: 1,
-        },
+    parent: {
+      type: Object
     },
-    data() {
-        return {
-            subListVisibility: false,
-            layerIsSelected: false,
-        };
-    },
-    computed: {
-        switchModel: {
-            get() {
-                if (this.data.hasOwnProperty("collapseVisibility")) {
-                    return this.data.collapseVisibility;
-                } else {
-                    return this.data.defaultVisibility;
-                }
-            },
-            set(e) {
-                if (e.target.checked === false) {
-                    this.subListVisibility = false;
-                }
+    loop: {
+      type: Number,
+      default: 1
+    }
+  },
+  data() {
+    return {
+      subListVisibility: false,
+      layerIsSelected: false
+    };
+  },
+  computed: {
+    switchModel: {
+      get() {
+        if (this.data.hasOwnProperty("collapseVisibility")) {
+          return this.data.collapseVisibility;
+        } else {
+          return this.data.defaultVisibility;
+        }
+      },
+      set(e) {
+        if (e.target.checked === false) {
+          this.subListVisibility = false;
+        }
 
-                this.selectLayer(e);
-            },
-        },
-        paddingLeft() {
-            return this.loop * 16 + "px";
-        },
-        showCaretIcons() {
-            return (
-                this.data.hasOwnProperty("children") ||
-                (this.data.layers && this.data.collapseVisibility)
-            );
-        },
-        showOperationIcons() {
-            return (
-                this.loop > 1 &&
-                !this.data.layers &&
-                !this.data.hasOwnProperty("children")
-            );
-        },
+        this.layerPicker(e);
+      }
     },
-    methods: {
-        toggleSubList() {
-            this.subListVisibility = !this.subListVisibility;
-        },
-        selectLayer(event) {
-            if (!this.data.hasOwnProperty("parentLayerId")) {
-                this.$emit(
-                    "selectLayer",
-                    this.data,
-                    this.data.order,
-                    this.data.mapType === "dynamic",
-                    event
-                );
-                console.log("selectLayer");
-            } else {
-                this.$emit(
-                    "selectSubLayer",
-                    this.parent,
-                    this.parent.order,
-                    this.data.id,
-                    event
-                );
-                console.log("selectSubLayer");
-            }
-        },
-        selectSubLayer(parent, order, itemId, event) {
-            this.$emit("selectSubLayer", parent, order, itemId, event);
-        },
-        getTableData(parent, layerId, layerName, query) {
-            console.log("yo");
-            this.$emit("getTableData", parent, layerId, layerName, query);
-        },
+    paddingLeft() {
+      return this.loop * 16 + "px";
     },
+    isCategory() {
+      return this.data.hasOwnProperty("children");
+    },
+    showCaretIcons() {
+      if (this.data.collapseVisibility)
+        console.log("collapse vis: ", this.data);
+      return (
+        this.isCategory || (this.data.layers && this.data.collapseVisibility)
+      );
+    },
+    showOperationIcons() {
+      return (
+        this.data.mapType !== "basemap" &&
+        this.loop > 1 &&
+        !this.data.layers &&
+        !this.isCategory
+      );
+    }
+  },
+  methods: {
+    toggleSubList() {
+      this.subListVisibility = !this.subListVisibility;
+    },
+
+    layerPicker(event) {
+      if (!this.data.hasOwnProperty("parentLayerId")) {
+        this.$emit(
+          "selectLayer",
+          this.data,
+          this.data.order,
+          this.data.mapType === "dynamic",
+          event
+        );
+      } else {
+        this.$emit(
+          "selectSubLayer",
+          this.parent,
+          this.parent.order,
+          this.data.id,
+          event
+        );
+      }
+    },
+    selectLayer(item, order, itemId, event) {
+      this.$emit("selectLayer", item, order, itemId, event);
+    },
+    selectSubLayer(parent, order, itemId, event) {
+      this.$emit("selectSubLayer", parent, order, itemId, event);
+    },
+    getTableData(parent, layerId, layerName, query) {
+      this.$emit("getTableData", parent, layerId, layerName, query);
+    }
+  }
 };
 </script>
